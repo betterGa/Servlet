965e666dcebe55609a6e598b77939959ab78bedf
yourjia <2086543608@qq.com>
2019-07-17 09:20:44 +0800

    类实现Servet接口，通过tomcat中的webapps手动部署

---
+++ JulyServer/src/com/MyServer/FirstServlet.java
@@ -0,0 +1,34 @@
+package com.MyServer;
+
+import java.io.IOException;
+import javax.servlet.*;
+public class FirstServlet implements Servlet{
+
+
+    @Override
+    public void init(ServletConfig arg0)throws ServletException
+    {//TODO Auto-generated method stub
+    }
+
+
+    @Override
+    public ServletConfig getServletConfig()
+    {//TODO Auto-generated method stub
+        return null; }
+
+    @Override
+    public void service(ServletRequest arg0,ServletResponse arg1) throws IOException {
+        //TODO Auto-generated method stub
+        arg1.getWriter().println("MyServletohh");
+    }
+
+    @Override
+    public String getServletInfo()
+    {//TODO Auto-generated method stub
+        return null; }
+
+    @Override
+    public void destroy()
+    {//TODO Auto-generated method stub
+        }
+   }
